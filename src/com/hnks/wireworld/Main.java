3e852c4c3a47d947eeab29cf6775a0c1fedabd94
scintilla4evr <kacper17@outlook.com>
2020-05-22 18:23:53 +0200

    UI theming - a journey

--- src/com/hnks/wireworld/Main.java
+++ src/com/hnks/wireworld/Main.java
@@ -5,9 +5,23 @@ import com.hnks.wireworld.gui.AppFrame;
 import javax.swing.*;
 import javax.swing.plaf.metal.DefaultMetalTheme;
 import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) {
+        SynthLookAndFeel theme = new SynthLookAndFeel();
+
+        try {
+            theme.load(
+                    Main.class.getResourceAsStream("gui/theme/theme.xml"),
+                    Main.class
+            );
+            UIManager.setLookAndFeel(theme);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         new AppFrame();
     }
 }
